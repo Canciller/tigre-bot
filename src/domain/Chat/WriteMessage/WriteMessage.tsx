04ebd8163c5633b86c28c5cfc9610199780765b3
Canciller <lopez.ojeda.gabriel.emilio@gmail.com>
2021-03-11 21:14:05 -0600

    Updated WriteMessage component

--- src/domain/Chat/WriteMessage/WriteMessage.tsx
+++ src/domain/Chat/WriteMessage/WriteMessage.tsx
@@ -1,12 +1,26 @@
-import React, { FunctionComponent } from 'react';
+import React, { FunctionComponent, useState } from 'react';
 import styles from './WriteMessage.module.scss';
 import SendIcon from '../../../components/SendIcon';
 
-const WriteMessage: FunctionComponent = () => {
+export type OnMessageWritten = (text: string) => void;
+
+interface WriteMessageProps {
+  onMessageWritten?: OnMessageWritten;
+}
+
+const WriteMessage: FunctionComponent<WriteMessageProps> = ({ onMessageWritten }: WriteMessageProps) => {
+  const [text, setText] = useState('');
+
   return (
     <div className={styles.root}>
-      <input className={styles.message} type="text" />
-      <div className={styles.send}>
+      <input onChange={(e) => setText(e.target.value)} className={styles.message} type="text" />
+      <div
+        onClick={(e) => {
+          if (onMessageWritten) onMessageWritten(text);
+          e.preventDefault();
+        }}
+        className={styles.send}
+      >
         <SendIcon />
       </div>
     </div>
